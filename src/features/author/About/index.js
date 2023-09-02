86704435a2a24e9b62d085118f82a19991507ddc
Kornel Kulwas <90424045+Speecter@users.noreply.github.com>
2023-09-02 13:25:25 +0200

    Add files via upload

---
+++ src/features/author/About/index.js
@@ -0,0 +1,17 @@
+import { AboutContent } from "./styled";
+
+
+const About = () => {
+  return (
+    <AboutContent>
+      <p>
+        <strong>Jestem uczniem liceum</strong>, który w wolnym czasie lubi nieco
+        odpocząć od codziennej rutyny. W wolnym czasie lubię grać w
+        <strong>gry komputerowe</strong> (także tworzyć modyfikacje do nich), a także
+        <strong>jeździć na rowerze</strong>. 
+      </p>
+    </AboutContent>
+  );
+};
+
+export default About;
---
+++ src/features/author/Author.js
@@ -0,0 +1,11 @@
+import { Container } from "../../common/Container/styled";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+import About from "./About";
+
+export default () => (
+  <Container>
+    <Header title="O autorze" />
+    <Section title="Kornel Kulwas" body={<About />} />
+  </Container>
+);
